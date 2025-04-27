8e4a7d6b2b4d19bba6f3aa926a84fca0d86573d8
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-04-27 17:59:24 +0600

    Add files via upload

---
+++ 3392. Count Subarrays of Length Three With a Condition.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public:
+        int countSubarrays(vector<int>& nums) {
+            int n = nums.size();
+            int cnt  = 0;
+    
+            for (int i=2; i<n; i++) {
+                if (nums[i] + nums[i-2] == (nums[i-1])/2.0) {
+                    cnt++;
+                }
+            }
+            return cnt ;
+        }
+    };
